b34286c507fc2767219549c8fbff2d6ae4c6cdcc
Timur Ismagilov <ismagilov42@yandex.ru>
2020-02-28 17:52:20 +0300

    Подключился к Mongo

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const { PORT, cardsRouter, usersRouter } = require('./config');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/cards', cardsRouter);
