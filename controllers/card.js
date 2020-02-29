6118ff8476fa91b23e2a8973d70b79203cd35df6
Timur Ismagilov <ismagilov42@yandex.ru>
2020-02-29 13:20:40 +0300

    Создал контроллеры для карточек

---
+++ controllers/card.js
@@ -0,0 +1,28 @@
+const Card = require('../models/card');
+
+const getCards = (req, res) => {
+  Card.find({})
+    .then(cards => res.send({data: cards}))
+    .catch(() => res.status(500).send({ message: 'Что-то пошло не так' }));
+};
+
+const createCard = (req, res) => {
+  const { name, link, likes, createdAt } = req.body;
+  const owner = req.user._id;
+
+  Card.create({ name, link, owner, likes, createdAt })
+    .then(card => res.send({data: card}))
+    .catch(() => res.status(500).send({ message: 'Что-то пошло не так' }));
+};
+
+const deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.id)
+    .then(card => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Что-то пошло не так' }));
+};
+
+module.exports = {
+  getCards,
+  createCard,
+  deleteCard
+};
