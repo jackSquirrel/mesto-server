e618940f792aa886aebc815a2a2831ef5b9018d4
Timur Ismagilov <ismagilov42@yandex.ru>
2020-03-11 15:45:14 +0300

    API не возвращает хэш пароля

--- models/user.js
+++ models/user.js
@@ -37,13 +37,14 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    select: false
   }
 },
 { versionKey: false });
 
 userSchema.statics.findUserByCredentials = function(email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then(user => {
       if(!user){
         return Promise.reject(new Error('Неправильный email или пароль'))
