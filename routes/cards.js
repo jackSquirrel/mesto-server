5c5125deb3114ee94fa64c3d202cb2f562beb2cd
Timur Ismagilov <ismagilov42@yandex.ru>
2020-03-31 15:37:17 +0300

    Валидация тел запросов

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { getCards, createCard, deleteCard, setLike, removeLike } = require('../controllers/card');
 
 router.get('/', getCards);
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    // eslint-disable-next-line no-useless-escape
+    link: Joi.string().required().regex(/https?:\/\/(www\.)?(?:[-\w.]+\.[a-z]+)(?:\/[-\w@\/]*#?)?(?:.(?:jpg|jpeg|png))?/)
+  })
+}), createCard);
 router.delete('/:cardId', deleteCard);
 router.put('/:cardId/likes', setLike);
 router.delete('/:cardId/likes', removeLike);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { getUsers, getUser, refreshProfile, refreshAvatar } = require('../controllers/user');
 
 router.get('/', getUsers);
 router.get('/:userId', getUser);
-router.patch('/me', refreshProfile);
-router.patch('/me/avatar', refreshAvatar);
+router.patch('/me', celebrate({
+  name: Joi.string().min(2).max(30),
+  about: Joi.string().min(2).max(30)
+}), refreshProfile);
+router.patch('/me/avatar', celebrate({
+  // eslint-disable-next-line no-useless-escape
+  avatar: Joi.string().required().regex(/https?:\/\/(www\.)?(?:[-\w.]+\.[a-z]+)(?:\/[-\w@\/]*#?)?(?:.(?:jpg|jpeg|png))?/)
+}), refreshAvatar);
 
 module.exports = router;
