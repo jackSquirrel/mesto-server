7325615c81b106b77efd507d2dbd228f3063f10c
Timur Ismagilov <ismagilov42@yandex.ru>
2020-03-31 23:15:40 +0300

    Настроил логгирование

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { celebrate, Joi, errors } = require('celebrate');
+
 const { createUser } = require('./controllers/user');
 const { login } = require('./controllers/user');
 const { auth } = require('./middlewares/auth');
 const { errorMiddleware } = require('./middlewares/errorMiddleware');
-
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT, cardsRouter, usersRouter } = require('./config');
 
 const app = express();
@@ -22,6 +23,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(requestLogger);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
@@ -33,16 +35,13 @@ app.post('/signup', celebrate({
     password: Joi.string().required()
   })
 }), createUser);
-
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().regex(/[-.\w]+@[-\w]+\.[a-z]+/),
     password: Joi.string().required()
   })
 }), login);
-
 app.use(auth);
-
 app.use('/cards', cardsRouter);
 app.use('/users', usersRouter);
 app.use('/*', (req, res, next) => {
@@ -52,6 +51,7 @@ app.use('/*', (req, res, next) => {
   });
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorMiddleware);
 
