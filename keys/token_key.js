c03a0e312b04cc9a2af52ebfd15d9e989eb1be81
Timur Ismagilov <ismagilov42@yandex.ru>
2020-04-01 20:45:20 +0300

    Настроил работу с env переменными

--- keys/token_key.js
+++ keys/token_key.js
@@ -1,1 +1,6 @@
-module.exports.key = 'some-secret-key';
+const { NODE_ENV, JWT_SECRET } = process.env;
+if (NODE_ENV === 'production') {
+  module.exports.key = JWT_SECRET;
+} else {
+  module.exports.key = 'some-secret-key';
+}
