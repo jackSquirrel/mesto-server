7a4492453b0ab4b3e63d13f36500fc7525477ce5
Timur Ismagilov <ismagilov42@yandex.ru>
2020-04-01 20:53:04 +0300

    Добавил краш-тест сервера

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
